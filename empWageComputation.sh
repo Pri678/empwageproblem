d6ca56ed90a7706427cdeea9f3074f06abae0452
Pri678 <priyasharma209206@gmail.com>
2021-07-28 19:18:36 +0530

    Added:Emp wage Attendance feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,16 @@
 #! /bin/bash -x
 echo "Welcome to empWageProblem"
+#! /bin/bash -x
+#constant variable
+IS_PRESENT=1
+
+#variable
+randomCheck=$((RANDOM%2))
+
+#selection
+if [ $randomCheck -eq 1 ]
+then
+      echo "Employe is present"
+else
+       echo "Employe is absent"
+fi
